a230e6d044c257d910b38528270940b6c403b12b
Parker Duncan <parkerduncan1009@gmail.com>
2022-05-17 23:23:23 -0500

    added dice roller

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 //collapsible notes scripting
+
+//Dice roller
+document.getElementById('diceRollButton').addEventListener('click', rollDice)
+
+function rollDice(){
+    let num = document.getElementById('nOfDice').value
+    let sides = document.getElementById('nOfSides').value
+    let result = []
+    for(let i = 0; i < num; i++){
+    result.push(Math.ceil(Math.random() * sides))
+    }
+    console.log(result)
+    document.getElementById('diceResults').innerText = result
+    }
+    
